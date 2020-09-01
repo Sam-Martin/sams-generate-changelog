b468f099d158854e416de93458f30125d154c265
Sam Martin <samjackmartin@gmail.com>
2020-09-01 17:22:36 +0100

    Further categorisation of author template

--- tests/test_generatechangelog.py
+++ tests/test_generatechangelog.py
@@ -16,7 +16,7 @@ class TestGenerateChangelog(unittest.TestCase):
     def setUp(self):
         self.default_args = {
             'old_version': '0520826f8057485f8f86f7198149c7b4ea6b6aa2',
-            'new_version': '447093540a6ca4719add4d738a85fcb7f50d88be',
+            'new_version': 'ac77514f027554af76506833825d418e5072a866',
             'git_path': '..'
             # 'custom_attributes': {
             #     'jira_id': {
@@ -26,34 +26,34 @@ class TestGenerateChangelog(unittest.TestCase):
             # }
         }
 
-    def test_render_markdown_change_type_template(self):
+    def test_render_markdown_change_type_all_commits_template(self):
         generate_changelog = GenerateChangelog(
-            template_name='change_type', **self.default_args)
+            template_name='change_type_all_commits', **self.default_args)
 
         result = generate_changelog.render_markdown()
 
-        with open(f'{TEST_FOLDER}/fixtures/change_type_template.md') as reader:
+        with open(f'{TEST_FOLDER}/fixtures/change_type_all_commits_template.md') as reader:
             print(result)
             assert result == reader.read()
 
     def test_render_markdown_author_template(self):
         generate_changelog = GenerateChangelog(
-            template_name='author', group_by='author', **self.default_args)
+            template_name='author_all_commits', group_by='author', **self.default_args)
 
         result = generate_changelog.render_markdown()
 
-        with open(f'{TEST_FOLDER}/fixtures/author_template.md') as reader:
+        with open(f'{TEST_FOLDER}/fixtures/author_all_commits_template.md') as reader:
             print(result)
             assert result == reader.read()
 
-    def test_render_markdown_author_single_file_entry_template(self):
+    def test_render_markdown_author_by_change_type_template(self):
         generate_changelog = GenerateChangelog(
-            template_name='author_single_file_entry', group_by='author', **self.default_args
+            template_name='author_by_change_type', group_by='author', **self.default_args
         )
 
         result = generate_changelog.render_markdown()
 
-        with open(f'{TEST_FOLDER}/fixtures/author_single_file_entry_template.md') as reader:
+        with open(f'{TEST_FOLDER}/fixtures/author_by_change_type_template.md') as reader:
             print(result)
             assert result == reader.read()
 
