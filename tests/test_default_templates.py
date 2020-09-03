b815f38f1f737da99711b2ed622e9e158ebab5f4
Sam Martin <samjackmartin@gmail.com>
2020-09-03 09:57:51 +0100

    Added CI pre-reqs

--- tests/test_default_templates.py
+++ tests/test_default_templates.py
@@ -8,7 +8,7 @@ DEFAULT_ARGS = {
     'start_ref': '0520826f8057485f8f86f7198149c7b4ea6b6aa2',
     'end_ref': 'ac77514f027554af76506833825d418e5072a866',
     'header_text': '1.0.0',
-    'git_path': '..'
+    'git_path': os.path.join(TEST_FOLDER, '..')
 }
 
 
